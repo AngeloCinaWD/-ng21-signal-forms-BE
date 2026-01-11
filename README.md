3d07842c81a264d785afa8d3d9d01767e9882652
AngeloCinaWD <angelo.cina@yahoo.it>
2026-01-11 17:07:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ng21SignalFormsBE
+
+This project was generated using Node 24.12 e npm 11.6.2
+
+## [thksTo](https://www.youtube.com/watch?v=dUQ3NV3mDb0)
+
+- create file server.js
+- initialize npm in project folder (npm init -y)
+- install express (npm install express)
---
+++ server.js
@@ -0,0 +1,41 @@
+// importiamo express dal modulo express
+import express from 'express';
+
+// creo l'applicazione
+const app = express();
+
+// creo la porta
+const port = 3002;
+
+// creo gli utenti già presenti e che quindi non sarà possibile prendere
+// passando un array con valori duplicati e creando da questo un Set questi verranno rimossi (ci sarà un solo mario)
+const takenUserNames = new Set([
+  'riccardo',
+  'mario',
+  'admin',
+  'mario',
+  'luigi',
+]);
+
+console.log(takenUserNames);
+
+// costruisco la route per il check dello username
+app.get('/api/check-username', (req, res) => {
+  // ricavo lo username passato come query param
+  const username = String(req.query.username || '').toLowerCase();
+
+  // dato che ho un Set utilizzo il metodo .has() per sapere se contiene quell'elemento
+  const available = !takenUserNames.has(username);
+
+  // simulo una latenza
+  setTimeout(() => {
+    res.json({ available });
+  }, 500);
+});
+
+// avvio l'app con un messaggio in console di avvio del server
+app.listen(port, () => {
+  console.log(`API server listening on http://localhost:${port}`);
+});
+
+// per avviare il BE digito node server.js
